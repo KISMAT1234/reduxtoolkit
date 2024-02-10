2d73d38b9feae7703f59deb3dfba7046c90048e9
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-10 15:28:39 +0545

    ch

--- project/src/main.jsx
+++ project/src/main.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Provider} from "react-redux"
-import store from "./store/index.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>,
 )
