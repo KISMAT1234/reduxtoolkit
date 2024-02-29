f2f0f6a8d7ab54cdaf64347adb1641730fbab18c
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-29 22:59:42 +0545

    react added

--- rtkquery/src/component/PostCard.tsx
+++ rtkquery/src/component/PostCard.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-const PostsCard = ({post}:{post:Post})=>{
+const PostsCard = ({post}:{post: Post})=>{
     return (
-    <>
+    <div>
         <h1>{post.title}</h1>
         <h1>{post.body}</h1>
-    </>
-    )
-}
+    </div>
+    );
+};
 
 export default PostsCard;
