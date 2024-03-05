b17b5e5e4cf58772b8d2e7fcedb66e842cdf976d
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-05 12:54:14 +0545

    addding todo

--- todolist/src/components/Todos.jsx
+++ todolist/src/components/Todos.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import {useSelector, useDispatch} from 'react-redux';
 import {removeTodo} from '../features/todo/todoSlice'
-const Todos = () => {
+const Todos = (id) => {
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
+    dispatch(removeTodo(id))
   return (
 <>
-        {todos.map((todo)=>(
+        {todos.map((todo, id)=>(
             <li key={todo.id}>
                 {todo.text}
-            <button onClick={()=>dispatch(removeTodo(todo.id))}>remove</button>
+            <button onClick={()=>dispatch(removeTodo(id))}>remove</button>
             </li>
         ))}
 </>    
