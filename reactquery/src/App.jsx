8fe97317205923b6eed65a9b3effd7302e8801da
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-07 11:44:53 +0545

    consoling data using usequery

--- reactquery/src/App.jsx
+++ reactquery/src/App.jsx
@@ -2,6 +2,12 @@
 
 function App() {
 
+  const { data, isLoading} =useQuery({
+    querykey:["posts"],
+    queryFn:fetchPosts,
+  })
+  console.log(data,isLoading)
+
   return (
     <>
       <h1>Kismat running</h1>
