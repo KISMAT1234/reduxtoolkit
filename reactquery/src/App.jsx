efdf89570ec3ca2ba57932085447db332652f179
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-07 12:03:12 +0545

    fetcing data through usequery

--- reactquery/src/App.jsx
+++ reactquery/src/App.jsx
@@ -1,12 +1,13 @@
 
-
+import {useQuery} from "@tanstack/react-query"
+import {fetchPosts} from "./api/api"
 function App() {
 
   const { data, isLoading} =useQuery({
     querykey:["posts"],
     queryFn:fetchPosts,
   })
-  console.log(data,isLoading)
+  console.log(data, isLoading)
 
   return (
     <>
