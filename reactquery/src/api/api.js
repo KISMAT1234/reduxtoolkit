624c6f07b6d28fa07a8b013e7116fb367a09aab7
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-07 12:08:29 +0545

    api link to send data to usequery

---
+++ reactquery/src/api/api.js
@@ -0,0 +1,31 @@
+const fetchPosts = async (page) => {
+    const response = await fetch(
+      `http://localhost:3000/posts?_sort=-id&`);
+  
+    if (!response.ok) {
+      throw new Error(`Failed to fetch posts. Status: ${response.status}`);
+    }
+  
+    const postData = await response.json();
+    return postData;
+  };
+  
+  const fetchTags = async () => {
+    const response = await fetch("http://localhost:3000/tags");
+    const tagsData = await response.json();
+    return tagsData;
+  };
+  
+  const addPost = async (post) => {
+    const response = await fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(post),
+    });
+  
+    return response.json();
+  };
+  
+  export {fetchPosts, fetchTags, addPost};
